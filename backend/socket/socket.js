4f53d6ecffdbaa9bf61960339c652325920f02b3
HimeshSrivastava <153629881+HimeshSrivastava@users.noreply.github.com>
2025-02-04 00:50:33 +0530

    changes

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -8,6 +8,7 @@ const io = new Server(server, {
     cors: {
         origin: ["http://localhost:5174","http://localhost:5173", "https://chat-app-frontend-new.onrender.com"],
         methods: ["GET", "POST", "PUT", "DELETE"],
+        credentials: true,
     },
 });
 
